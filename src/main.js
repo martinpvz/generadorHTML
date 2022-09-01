9c32ec837b9a0e0510bc77388e6cfc4bc42f5a8d
martinpvz <martin.paniagua200013@gmail.com>
2022-08-31 22:39:55 -0500

    se agregan documentos

---
+++ src/main.js
@@ -0,0 +1,56 @@
+// $(document).ready(function(){
+//   $('ul.first').bsPhotoGallery({
+//     "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+//     "hasModal" : true,
+//     // "fullHeight" : false
+//   });
+// });
+
+
+function comprar() {
+  window.alert("Dirige a la pagina de compra")
+
+}
+
+
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
